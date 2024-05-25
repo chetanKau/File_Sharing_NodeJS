37054dc54e7514d77c32cd15f7f90ebf4c1376a7
Chetan kaushik <chetankaushik29@gmail.com>
2024-05-25 20:16:57 +0530

    Downloadable link and file download link is working fine

--- controllers/file.controller.js
+++ controllers/file.controller.js
@@ -15,7 +15,7 @@ const storage = multer.diskStorage({
     },
     filename: (req, file, cb) => {
         console.log(file.originalname);
-        const fileName = (file.originalname) + uuidv4() + path.extname(file.originalname)
+        const fileName = uuidv4() + path.extname(file.originalname)
         cb(null, fileName)
     }
 
@@ -28,7 +28,7 @@ const upload = multer({
 const uploadFile = async (req, res) => {
     upload(req, res, async (error) => {
         if (error) {
-            console.log("File upload process is having error",error);
+            console.log("File upload process is having error", error);
             return;
         }
 
@@ -55,19 +55,51 @@ const uploadFile = async (req, res) => {
 
 const generateDynamicLink = async (req, res) => {
 
+    try {
+        const fileId = req.params.uuid;
+        // console.log(fileId);
+        const file = await FileModel.findById(fileId.toHexString)
 
-    res.json({
-        status: "success",
-        message: "File downloaded successfully"
-    })
+        if (!file) {
+            return res.status(404).json({
+                status: false,
+                message: "File not Found"
+            })
+                
+        }
+        res.json({
+            status: "success",
+            message: "File downloaded link generated successfully",
+            linktoDownload: 'http://localhost:4080/files/download/' + fileId,
+        })
+
+
+    } catch (error) {
+        console.log("Error Found", error.message);
+        res.status(500).json({
+            status: false,
+            message: error.message,
+        });
+    }
+
 }
 
 const downloadFileUUID = async (req, res) => {
 
-    res.json({
-        status: "success",
-        message: "File download File with UUID successfully"
-    })
+    try {
+        const fileId = req.params.uuid;
+        // console.log(fileId);
+        const file = await FileModel.findById(fileId)
+
+        if (!file) {
+
+            return res.end("File with given id not found")
+        }
+        res.download(file.path,file.originalFileName)
+
+    } catch (error) {
+        res.end("Something went wrong, please try again after some time.")
+    }
 }
 
 const sendEmailDownloadLink = async (req, res) => {
